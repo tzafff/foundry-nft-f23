f368a497c87170ee60f1ad32132b252180fe8e34
tzafff <christzaf99@gmail.com>
2024-05-08 13:55:50 +0300

    Nft Contract/ Deploy Script

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -5,5 +5,21 @@ pragma solidity ^0.8.19;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
-    constructor() ERC721("BasicNft", "DOG") {}
+    uint256 private s_tokenCounter;
+    mapping (uint256 => string) private s_tokenIdToUri;
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+    
+    function tokenURI (uint256 tokendId) public view override returns (string memory) {
+        return s_tokenIdToUri[tokendId];
+    }
 }
